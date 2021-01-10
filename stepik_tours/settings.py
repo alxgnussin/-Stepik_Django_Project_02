38ca2c997b74dc7fc28951dc604a55c9aa97a51e
alxgnussin <alxgns@yandex.ru>
2021-01-10 18:55:43 +0700

    staticfiles bug fix.

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = os.environ.get('HOSTS', '127.0.0.1').split(',')
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
